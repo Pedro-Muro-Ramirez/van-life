12bbfb8c3aebcfa139c92a64a5259b5f0f647362
Pedro-Muro-Ramirez <84997689+Pedro-Muro-Ramirez@users.noreply.github.com>
2025-12-10 09:30:51 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import './App.css'
+import Home from './pages/Home'
+import About from './pages/About'
+import Vans from './pages/Vans/Vans'
+import VanDetail from './pages/Vans/VanDetail'
+import Layout from './components/Layout'
+import HostLayout from './components/HostLayout'
+import Dashboard from './pages/Host/Dashboard'
+import Income from './pages/Host/Income'
+import Reviews from './pages/Host/Reviews'
+import HostVans from './pages/Host/HostVans'
+import HostVanDetail from './pages/Host/HostVanDetail'
+import HostVanInfo from './pages/Host/HostVanInfo'
+import HostVanPhotos from './pages/Host/HostVanPhotos'
+import HostVanPricing from './pages/Host/HostVanPricing'
+import NotFound from './pages/NotFound'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import './server'
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route path='*' element={<NotFound />} />
+        <Route index element={<Home />}/>
+        <Route path='/about' element={<About />} /> 
+        <Route path='/vans' element={<Vans />} />
+        <Route path='/vans/:id' element={<VanDetail />} /> 
+        <Route path='/host' element={<HostLayout />}>
+          <Route index element={<Dashboard />} /> 
+          <Route path='income' element={<Income />} /> 
+          <Route path='reviews' element={<Reviews />} /> 
+          <Route path='vans' element={<HostVans />} /> 
+          <Route path='vans/:id' element={<HostVanDetail />} >
+            <Route index element={<HostVanInfo />} />
+            <Route path='pricing' element={<HostVanPricing />} />
+            <Route path='photos' element={<HostVanPhotos />} />
+          </Route>
+        </Route>
+      </Route>
+    </Routes>
+  </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/pages/Vans/Vans.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import { Link, useSearchParams } from 'react-router-dom'
+import { getVans } from '../../api'
+
+export default function Vans() {
+    const [searchParams, setSearchParams] = useSearchParams()
+    const [vans, setVans] = React.useState([])
+    const [loading, setLoading] = React.useState(false)
+    const [error, setError] = React.useState(null)
+
+    const typeFilter = searchParams.get("type")
+
+    React.useEffect(() => {
+        async function loadVans() {
+            setLoading(true)
+            try {
+                const data = await getVans()
+                setVans(data)
+            } catch(err) {
+                console.log(err)
+                setError(err)
+                return
+            } finally {
+                setLoading(false)
+            }
+        }
+        loadVans()
+    }, [])
+
+
+    if (loading) {
+        return <h1>Loading...</h1>
+    }
+
+    if (error) {
+        return <pre>{JSON.stringify(error, null, 2)}</pre>
+    }
+
+
+    const displayedVans = typeFilter
+    ? vans.filter((van) => van.type === typeFilter)
+    : vans
+
+    const vanElements = displayedVans.map((van) => {
+        return (
+            <div key={van.id} className='van-tile'>
+                <Link to={van.id} state={{ search: `?${searchParams.toString()}`, type: typeFilter }}>
+                    <img src={van.imageUrl} /> 
+                    <div>
+                        <h3>{van.name}</h3>
+                        <p>${van.price}<span>/day</span></p>
+                    </div>
+                    <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                </Link>
+            </div>
+        )
+    })
+
+
+
+    
+
+    return(
+        <div className='van-list-container'>
+            <h1>Explore our van options</h1>
+            <div className="van-list-filter-buttons">
+            <button onClick={() => setSearchParams({type: "simple"})} className={`van-type simple ${typeFilter === "simple" ? "selected" : ""}`}>Simple</button>
+            <button onClick={() => setSearchParams({type: "rugged"})} className={`van-type rugged ${typeFilter === "rugged" ? "selected" : ""}`}>Rugged</button>
+            <button onClick={() => setSearchParams({type: "luxury"})} className={`van-type luxury ${typeFilter === "luxury" ? "selected" : ""}`}>Luxury</button>
+           { typeFilter && <button onClick={() => setSearchParams({})} className={`van-type clear-filters`}>Clear filter</button>}
+            </div>
+            <div className='van-list'>
+                {vanElements}
+            </div>
+        </div>
+            
+    )
+}
